25c49bc2233e11ccd178d7c158393c3f6c136749
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-03-08 17:47:25 +0100

    Creato Start Menu, Game Over e Level 1 Scenes. Creato il SceneLoader. Fatta la parte di grafica dello Start Menu e Game Over. Fatto solo il background di Level 1.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// Carica la scena di partenza del gioco.
+    /// </summary>
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    /// <summary>
+    /// Carica la scena successiva a qualsiasi sia quella attuale.
+    /// </summary>
+    public void LoadNextScene()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+    }
+
+    /// <summary>
+    /// Esce dal gioco.
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
